a4faa146cdfbf63c44eec26330b5ca49deef342f
mosi <mosi@mosi.com>
2021-05-25 18:19:49 +0430

    new pages

---
+++ pages/contactus.js
@@ -0,0 +1,36 @@
+import Link from 'next/link'
+import styles from '../styles/Home.module.css'
+
+
+export default function Contact() {
+    return (
+      <div className={styles.container}>
+        <main className={styles.main}>
+          <h1 className={styles.title}>
+            <p>Contact</p>
+            <p>Erfan GT</p>
+  
+          </h1>
+          <div className={styles.grid}>
+            <Link href="/aboutus">
+                <a className={styles.card}> درباره ما</a>
+            </Link>
+            <Link href="/">
+                <a className={styles.card}>صفحه اصلی</a>
+            </Link>
+          </div>
+        </main>
+  
+        <footer className={styles.footer}>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Powered by{' '}
+            <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          </a>
+        </footer>
+      </div>
+    )
+  }
--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,22 @@
 import styles from '../styles/Home.module.css'
+import Link from 'next/link'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-	  <p>وزین و دیگر هیچ</p>
+          Welcome to Vazin!
+          <p>A market place for plastic trading</p>
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
         <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+            <Link href="/aboutus">
+                <a className={styles.card}> درباره ما</a>
+            </Link>
+            <Link href="/contactus">
+                <a className={styles.card}> تماس ما</a>
+            </Link>
+          </div>
       </main>
 
       <footer className={styles.footer}>
